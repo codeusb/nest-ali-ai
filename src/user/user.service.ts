475c489fd781f04ff06577cf8781a8553ecad5a6
Sexxion <2068610832@qq.com>
2025-09-14 21:09:31 +0800

    1

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,7 +7,7 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserService {
